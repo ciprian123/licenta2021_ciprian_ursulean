ebcb8a97d58ecef68d2f579c2238e8a9238adaa6
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2021-06-23 21:35:24 +0300

    Some unused code cleanup

--- licenta/src/main/java/com/ciprianursulean/licenta/LicentaApplication.java
+++ licenta/src/main/java/com/ciprianursulean/licenta/LicentaApplication.java
@@ -1,6 +1,5 @@
 package com.ciprianursulean.licenta;
 
-import com.ciprianursulean.licenta.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -14,14 +13,5 @@ public class LicentaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicentaApplication.class, args);
-	}
-
-	@Bean
-	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
-		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
-		AuthFilter authFilter = new AuthFilter();
-		registrationBean.setFilter(authFilter);
-		registrationBean.addUrlPatterns("/api/v1/predictions/*");
-		return registrationBean;
 	}
 }
